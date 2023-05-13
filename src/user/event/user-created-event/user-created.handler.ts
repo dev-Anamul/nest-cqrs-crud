61114391c8936c7389bce4dc8a95bb1825751e4a
dev-Anamul <ah9737677@gmail.com>
2023-05-13 13:19:57 +0600

    updload all the  file

---
+++ src/user/event/user-created-event/user-created.handler.ts
@@ -0,0 +1,13 @@
+import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
+import { User } from 'src/user/entity/user.entity';
+import { UserCreatedEvent } from './user-created.event';
+import { UserService } from 'src/user/entity/user.service';
+
+@EventsHandler(UserCreatedEvent)
+export class UserCreatedHandler implements IEventHandler<UserCreatedEvent> {
+  constructor(private readonly userService: UserService) {}
+  async handle(event: UserCreatedEvent) {
+    const user = event.user;
+    return await this.userService.create(user);
+  }
+}
